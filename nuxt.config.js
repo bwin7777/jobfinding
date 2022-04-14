15cf3ee439121f15013249540ded453ba2f1c16b
Nguyễn Nam Anh <namanh21896@gmail.com>
2022-04-15 00:10:26 +0700

    Fix: Fix nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,9 +16,6 @@ export default {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-  generate: {
-    dir: "public"
-  },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
